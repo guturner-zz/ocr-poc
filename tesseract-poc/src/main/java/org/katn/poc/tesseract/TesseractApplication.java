cfaf0879a0ba4849754d44a62de64d8ba6e01f7b
Turner <n0235650@lm.lmig.com>
2017-09-11 09:41:38 -0400

    Adding Tess4J POC.

---
+++ tesseract-poc/src/main/java/org/katn/poc/tesseract/TesseractApplication.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2017, Liberty Mutual Group
+ *
+ * Created on Sep 9, 2017
+ */
+
+package org.katn.poc.tesseract;
+
+import java.io.File;
+
+import net.sourceforge.tess4j.ITesseract;
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+
+/**
+ * @author n0235650
+ *
+ */
+public class TesseractApplication {
+
+	public static void main(String[] args) {
+		File imageFile = new File(getFilePath("sample_images/example.PNG"));
+		ITesseract instance = new Tesseract(); // JNA Interface Mapping
+
+		try {
+			String result = instance.doOCR(imageFile);
+			System.out.println(result);
+		} catch (TesseractException e) {
+			System.err.println(e.getMessage());
+		}
+	}
+	
+	private static String getFilePath(String path) {
+		return ClassLoader.getSystemResource(path).getPath();
+	}
+}
